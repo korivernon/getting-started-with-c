01f20674b3b72b486484dfe2218586e00b313128
Kori Vernon <korivernon@nyu.edu>
2020-08-21 12:14:19 -0400

    latest

---
+++ basics/basics.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int x;
+    x = 5;
+    int y = 9;
+    // format string 
+    printf("The magic number of x is: %i\nThe value of y is: %i\n",x,y);
+    return 0;
+}
